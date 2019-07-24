4f68b61b557c02274aa5d654917d3449a7a8faf9
Piotr Potocki <adefiks@gmail.com>
2019-07-24 16:35:38 +0200

    loading colliders from map

--- source/Game_Map.cpp
+++ source/Game_Map.cpp
@@ -20,8 +20,10 @@ void Game_Map::LoadMap(string path, int x, int y)
     int src_x, src_y;
     int tile_int = 0;
     string tile_string;
+    string title_tag = "clear";
     int multiplier = 1;
 
+    // tiles
     for (int i = 0; i < y; i++)
     {
         for (int j = 0; j < x; j++)
@@ -47,6 +49,54 @@ void Game_Map::LoadMap(string path, int x, int y)
                 mapFile.ignore();
         }
     }
+
+    mapFile.ignore();
+
+    // collisions
+    for (int i = 0; i < y; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
+            tile_string.clear();
+            do
+            {
+                mapFile.get(tile);
+                if (tile != ',')
+                    tile_string += tile;
+
+            } while (tile != ',');
+
+            tile_int = atoi(tile_string.c_str());
+
+            if (tile_int != 0)
+            {
+                switch (tile_int)
+                {
+                case 1:
+                    title_tag = "ground";
+                    break;
+                case 2:
+                    title_tag = "platform";
+                    break;
+                case 3:
+                    title_tag = "water";
+                    break;
+                default:
+                    title_tag = "ground";
+                    break;
+                }
+
+                auto &tile_collider(manager.addEntity());
+                tile_collider.addComponent<ColliderComponent>(title_tag, j * (tile_size * map_scale), i * (tile_size * map_scale), (tile_size * map_scale));
+                tile_collider.addGroup(Game::groupCollider);
+                // tile_collider.delGroup(Game::groupMap); // TODO ?? not sure if needed
+            }
+
+            if (j == (x - 1))
+                mapFile.ignore();
+        }
+    }
+
     mapFile.close();
 }
 

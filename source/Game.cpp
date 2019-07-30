1b2925c31a24f80f9127cfefc9f1062eea92ce82
Piotr Potocki <adefiks@gmail.com>
2019-07-30 17:47:55 +0200

    jump fixed

--- source/Game.cpp
+++ source/Game.cpp
@@ -106,17 +106,20 @@ void Game::update()
     // player position in this moment for bactracing player
     Vector2D player_position = player.getComponent<TransformComponent>().position;
 
-    manager.refresh();
-    manager.update();
-
     for (auto cc : colliders)
     {
         SDL_Rect c_collider = cc->getComponent<ColliderComponent>().collider;
         if (Collision::AABB(c_collider, player_collider))
         {
-            Collision::Collision_action(player.getComponent<TransformComponent>(), player_position, cc->getComponent<ColliderComponent>().tag);
+            if (!player.getComponent<KeyboardComponent>().jump_active)
+            {
+                Collision::Collision_action(player.getComponent<TransformComponent>(), player_position, cc->getComponent<ColliderComponent>().tag);
+            }
         }
     }
+
+    manager.refresh();
+    manager.update();
 
     camera.x = player.getComponent<TransformComponent>().position.x - (Game::resolution_width / 2);
     //camera.y = player.getComponent<TransformComponent>().position.y - (Game::resolution_height / 2);

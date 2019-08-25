f5310d694f6087132e445093bb8afdc118287a91
Piotr Potocki <adefiks@gmail.com>
2019-08-25 22:56:30 +0200

    sky

--- source/Game.cpp
+++ source/Game.cpp
@@ -56,7 +56,7 @@ void Game::init(const char *title, int xpos, int ypos, int width, int height, bo
         renderer = SDL_CreateRenderer(window, -1, 0);
         if (renderer)
         {
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_SetRenderDrawColor(renderer, 220, 251, 254, 255);
             cout << "renderer created!" << endl;
         }
         isRunning = true;
